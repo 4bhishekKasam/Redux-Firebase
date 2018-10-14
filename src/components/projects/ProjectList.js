ac951eb84cffb56ffbf726c4cdb0c4e76849c385
4bhishekKasam <konnurabhishek@gmail.com>
2018-10-14 17:52:13 +0530

    Mapping ProjectList

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,35 +1,13 @@
 import React from 'react'
+import ProjectSummary from './ProjectSummary';
+
 
 const ProjectList = () => {
     return (
         <div className="project-list section">
-            <div className="card z-depth-0 project-summary">
-                <div className="card-conent grey-text text-darken-3">
-                    <span className="card-title">
-                        Project Title
-                   </span>
-                    <p>Posted by the Net Ninja</p>
-                    <p className="grey-text">14th Sept, 12pm</p>
-                </div>
-            </div>
-            <div className="card z-depth-0 project-summary">
-                <div className="card-conent grey-text text-darken-3">
-                    <span className="card-title">
-                        Project Title
-                   </span>
-                    <p>Posted by the Net Ninja</p>
-                    <p className="grey-text">14th Sept, 12pm</p>
-                </div>
-            </div>
-            <div className="card z-depth-0 project-summary">
-                <div className="card-conent grey-text text-darken-3">
-                    <span className="card-title">
-                        Project Title
-                   </span>
-                    <p>Posted by the Net Ninja</p>
-                    <p className="grey-text">14th Sept, 12pm</p>
-                </div>
-            </div>
+            <ProjectSummary />
+            <ProjectSummary />
+            <ProjectSummary />
         </div>
     )
 }
---
+++ src/components/projects/ProjectSummary.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const ProjectSummary = () => {
+    return (
+        <div className="card z-depth-0 project-summary">
+            <div className="card-conent grey-text text-darken-3">
+                <span className="card-title">
+                    Project Title
+                  </span>
+                <p>Posted by the Net Ninja</p>
+                <p className="grey-text">14th Sept, 12pm</p>
+            </div>
+        </div>
+    )
+}
+
+export default ProjectSummary;
