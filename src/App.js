4ae5714ba3b03b48386a907aad1aea4a4a4c5431
4bhishekKasam <konnurabhishek@gmail.com>
2018-10-15 21:00:15 +0530

    SigIn and SingUp configuration

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Dashboard from './components/dashboard/Dashboard';
 import ProjectDetails from './components/projects/ProjectDetails';
+import SignUp from './components/auth/SignUp';
+import SignIn from './components/auth/SignIn';
 import './App.css';
 
 class App extends Component {
@@ -14,8 +16,8 @@ class App extends Component {
           <Switch>
             <Route exact path="/" component={Dashboard} />
             <Route path="/project/:id" component={ProjectDetails} />
-
-
+            <Route path="/signin" component={SignIn} />
+            <Route path="/signup" component={SignUp} />
           </Switch>
         </div>
       </BrowserRouter>
