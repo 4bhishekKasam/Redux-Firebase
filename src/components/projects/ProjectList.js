589234d0bd36462cdf95757290b0f41240a808ae
4bhishekKasam <konnurabhishek@gmail.com>
2018-10-22 21:28:00 +0530

    ProjectDetails linked to firestore

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ProjectSummary from './ProjectSummary';
-
+import { Link } from 'react-router-dom';
 
 const ProjectList = ({ projects }) => {
     return (
@@ -8,7 +8,10 @@ const ProjectList = ({ projects }) => {
             {
                 projects && projects.map(project => {
                     return (
-                        <ProjectSummary project={project} key={project.id} />
+                        <Link to={'/project/' + project.id}
+                            key={project.id}>
+                            <ProjectSummary project={project} />
+                        </Link>
                     )
                 })
             }
