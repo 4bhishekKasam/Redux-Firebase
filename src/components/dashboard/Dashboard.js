99a3cb1b6c17b70b469fa02b739802f582edc38f
4bhishekKasam <konnurabhishek@gmail.com>
2018-10-22 20:36:17 +0530

    get data from firebase

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { firestoreConnect } from 'react-redux-firebase';
+import { compose } from 'redux';
 import ProjectList from '../projects/ProjectList';
 import Notifications from './Notifications';
 
 class Dashboard extends Component {
     render() {
-       // console.log(this.props);
+        // console.log(this.props);
         const { projects } = this.props;
 
         return (
@@ -24,9 +26,15 @@ class Dashboard extends Component {
 }
 
 const mapStateToProps = (state) => {
+    // console.log(state);
     return {
-        projects: state.project.projects
+        projects: state.firestore.ordered.projects
     }
 }
 
-export default connect(mapStateToProps)(Dashboard);
+export default compose(
+    connect(mapStateToProps),
+    firestoreConnect([
+        { collection: 'projects' }
+    ])
+)(Dashboard);
