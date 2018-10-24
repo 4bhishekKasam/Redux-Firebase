a0bd6649aa2d1babafee0f4cf3c623360f3aa918
4bhishekKasam <konnurabhishek@gmail.com>
2018-10-24 19:32:44 +0530

    signout redux setup

--- src/store/reducers/authReducer.js
+++ src/store/reducers/authReducer.js
@@ -16,6 +16,10 @@ const authReducer = (state = initialState, action) => {
                 authError: null
             }
 
+        case 'SIGNOUT_SUCCESS':
+            console.log('signout success');
+            return state;
+
         default:
             return state;
     }
